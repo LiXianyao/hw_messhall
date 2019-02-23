88dfaa360a10632ac3a366e8499fb54f597564b5
LiXianyao <18976753003@163.com>
2019-02-23 19:57:57 +0800

    添加信息查询接口

--- src/main/java/edu/graduate/messhall/controller/infoController.java
+++ src/main/java/edu/graduate/messhall/controller/infoController.java
@@ -21,15 +21,12 @@ public class infoController {
 
     //handle the POST-/infoModify：检查重名-插入数据表-返回结果
     @RequestMapping(value = "/infoModify", method = {RequestMethod.GET, RequestMethod.POST})
-    public infoResponse modifyInfoRequest(@RequestBody infoEntity entity){
+    public infoResponse modifyInfoRequest(@RequestBody infoModifyEntity entity){
         int userId = entity.getUserId();
         String userName = entity.getUserName();
         String userPhone = entity.getUserPhone();
         int userAge = entity.getUserAge();
         int userGender = entity.getUserGender();
-
-        log.info("信息修改请求：" + userName + userPhone);
-        System.out.println(userName + userPhone);
 
         TblUser searchRes = tblUserRepository.findByUserName(userName);
         TblUser toModify = tblUserRepository.findByUserId(userId);
@@ -53,16 +50,44 @@ public class infoController {
         }
         return response;
     }
+
+    //handle the POST-/infoQuery：检查数据表-返回结果
+    @RequestMapping(value = "/infoQuery", method = {RequestMethod.GET, RequestMethod.POST})
+    public infoQueryResponse modifyInfoRequest(@RequestBody infoQueryEntity entity){
+        int userId = entity.getUserId();
+        TblUser user = tblUserRepository.findByUserId(userId);
+        return new infoQueryResponse(user);
+    }
 }
 
 
 @Data
-class infoEntity{
+class infoModifyEntity{
     private String userName;
     private String userPhone;
     private int userAge;
     private int userGender;
     private int userId;
+}
+
+@Data
+class infoQueryEntity{
+    private int userId;
+}
+
+@Data
+class infoQueryResponse{
+    private String userName;
+    private String userPhone;
+    private int userAge;
+    private int userGender;
+
+    infoQueryResponse(TblUser user){
+        this.userName = user.getUserName();
+        this.userAge = user.getUserAge();
+        this.userGender = user.getUserGender();
+        this.userPhone = user.getUserPhone();
+    }
 }
 
 @Data
