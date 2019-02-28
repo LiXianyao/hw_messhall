b6a2fdfe2425ab16a95fc5a2bcfc7f3ab5041faa
LiXianyao <buptlixianyao@163.com>
2019-02-28 10:53:11 +0800

    购物车数据库服务类调试，查询函数调整

--- src/main/java/edu/graduate/messhall/bean/TblCartRepository.java
+++ src/main/java/edu/graduate/messhall/bean/TblCartRepository.java
@@ -5,7 +5,10 @@ import org.springframework.data.jpa.repository.JpaRepository;
 import java.util.List;
 
 public interface TblCartRepository extends JpaRepository<TblCart, Integer> {
-    List<TblCart> findAllByUser(TblUser Belong);
+
+    List<TblCart> findAllByUser_UserId(int userId);
 
     TblCart findByUserAndFood(TblUser user, TblFood food);
+
+    TblCart findByUser_UserIdAndFood_FoodId(int userId, int foodId);
 }
