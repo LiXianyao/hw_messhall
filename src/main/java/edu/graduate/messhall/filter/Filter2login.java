4ab3f4c001879d1968fec24fa79cae656850ccb2
LiXianyao <buptlixianyao@163.com>
2019-03-05 23:41:38 +0800

    发现springboot的过滤器调用顺序是按类名的字典序，调整类名，并完善登陆验证过滤器

--- src/main/java/edu/graduate/messhall/filter/Filter2login.java
+++ src/main/java/edu/graduate/messhall/filter/Filter2login.java
@@ -9,10 +9,10 @@ import javax.servlet.http.*;
 import java.io.IOException;
 import edu.graduate.messhall.filter.commonFunction;
 
-//@WebFilter(urlPatterns = "/*", filterName = "loginFilter")
-//@Order(1)
-public class loginFilter extends HttpFilter {
-    private final Logger log = LoggerFactory.getLogger(loginFilter.class);
+@WebFilter(urlPatterns = "/*", filterName = "Filter2login")
+@Order(2)
+public class Filter2login extends HttpFilter {
+    private final Logger log = LoggerFactory.getLogger(Filter2login.class);
 
     private String[] allowedPatterns = {"/", "/login","/register"};
     private String[] allowedSuffiexs = {".js", ".css", ".ico", ".json"};
@@ -22,17 +22,18 @@ public class loginFilter extends HttpFilter {
         throws IOException, ServletException {
 
         String url = commonFunction.getRequestUrl(request);
-        log.info("=======拦截请求:"+  url + "======");
-        if(commonFunction.checkUriAllowence(url, allowedPatterns, allowedPatterns)){
+        if(commonFunction.checkUriAllowence(url, allowedPatterns, allowedSuffiexs)){
             filterChain.doFilter(request, response);// allowed url, just let go
             return;
         }
 
         //check the session to figure out weather the user is login or not
-        String userid = (String)request.getSession().getAttribute("user");
-        if(userid == null){ //if not redirect he to the '/login'page
-                log.info("被拦截：跳转到login页面");
-                request.getRequestDispatcher("/login").forward(request, response);
+        log.info("=======登陆验证拦截拦截请求:"+  url + "======");
+        String sessionId = request.getSession().getId();
+        String userid = (String)request.getSession().getAttribute("sessionId");
+        if(userid==null || !userid.equals(sessionId)){ //if not redirect he to the '/login'page
+                log.info("被拦截：跳转到loginRequire页面" + request.getSession().getId());
+                request.getRequestDispatcher("/loginRequire").forward(request, response);
         }else{// already login do nothing
             filterChain.doFilter(request, response);
         }
