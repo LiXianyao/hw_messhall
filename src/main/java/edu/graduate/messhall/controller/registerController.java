5eeb2d442b0985ee63da33612862c6de791cd584
LiXianyao <buptlixianyao@163.com>
2019-03-05 23:40:05 +0800

    删除一行无用引用

--- src/main/java/edu/graduate/messhall/controller/registerController.java
+++ src/main/java/edu/graduate/messhall/controller/registerController.java
@@ -1,7 +1,6 @@
 package edu.graduate.messhall.controller;
 
 import edu.graduate.messhall.bean.*;
-import edu.graduate.messhall.filter.loginFilter;
 import org.slf4j.Logger;
 import org.slf4j.LoggerFactory;
 import org.springframework.beans.factory.annotation.Autowired;
