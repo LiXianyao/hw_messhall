7ea0ff71561258b9d87945628f8db2f2a38f8fc4
LiXianyao <buptlixianyao@163.com>
2019-03-05 15:33:34 +0800

    数据库服务函数增加：按时间区间及订单状态字段的查询

--- src/main/java/edu/graduate/messhall/bean/TblOrderRepository.java
+++ src/main/java/edu/graduate/messhall/bean/TblOrderRepository.java
@@ -2,14 +2,21 @@ package edu.graduate.messhall.bean;
 
 import org.springframework.data.jpa.repository.JpaRepository;
 
+import java.util.Date;
 import java.util.List;
 
 public interface TblOrderRepository extends JpaRepository<TblOrder, Integer> {
-    TblOrder findByOrderId(String orderId);
+    TblOrder findByOrderId(int orderId);
 
     List<TblOrder> findAllByBusinessIdOrderByTimeAsc(int businessId);
 
     List<TblOrder> findAllByCustomerIdOrderByTimeAsc(int customerId);
 
     List<TblOrder> findAllByOrderByTimeAsc();
+
+    List<TblOrder> findAllByTimeBetweenAndStateEqualsOrderByTimeAsc(Date start, Date end, String status);
+
+    List<TblOrder> findAllByTimeBetweenAndBusinessIdAndStateEqualsOrderByTimeAsc(Date start, Date end, int businessId, String status);
+
+    List<TblOrder> findAllByTimeBetweenAndCustomerIdAndStateEqualsOrderByTimeAsc(Date start, Date end, int customerId, String status);
 }
